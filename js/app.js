7d523c441feb0febc3496b65d750a0d83685b8d0
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-06-16 09:56:38 +0300

    animat the experince section

--- js/app.js
+++ js/app.js
@@ -17,4 +17,22 @@ $(document).ready(function(){
         loopCount : Infinity,
         startDelay : 1000
     });
+
+    //progress bars
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function() {
+            let p = document.querySelectorAll('.progress-bar');
+            p[0].setAttribute("style", "width:100%;transition:1s all");
+            p[1].setAttribute("style", "width:95%;transition:1.5s all");
+            p[2].setAttribute("style", "width:85%;transition:1.7s all");
+            p[3].setAttribute("style", "width:99%;transition:2s all");
+            p[4].setAttribute("style", "width:85%;transition:2.3s all");
+            p[5].setAttribute("style", "width:95%;transition:2.5s all");
+        },
+        offset:'90%'
+      });
+
+    
 });
