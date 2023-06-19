f429534559384a551be75fc3c888bd72c10957be
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-06-19 23:20:42 +0300

    Animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $(window).on('load', function(){
+        $('.preloader').addClass('complete')
+    });
+
     $(window).on('scroll',function(){
         let scroll = $(window).scrollTop();
         if(scroll >= 50){
